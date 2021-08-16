576f40f4152d8504db6f527422b885f49ed4834e
pengzhong <189968924@qq.com>
2021-08-16 13:35:58 +0800

    归并排序

--- src/排序/归并排序/MergeSort.java
+++ src/排序/归并排序/MergeSort.java
@@ -7,8 +7,17 @@ public class MergeSort {
         int[] arr = {8, 4, 5, 7, 1, 3, 6, 2};
         //归并排序需要一个额外的空间
         sort(arr, 0, arr.length - 1, new int[arr.length]);
-
         System.out.println(Arrays.toString(arr));
+        //测试8万条数据都排序时间
+        int[] test = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            test[i] = (int) (Math.random() * 8000000);
+        }
+        //System.out.println(Arrays.toString(test));
+        long start = System.currentTimeMillis();
+        sort(test, 0, test.length - 1, new int[test.length]);
+        long end = System.currentTimeMillis();
+        System.out.println("耗费时间：" + (end - start));
     }
 
     //分+合的方法
