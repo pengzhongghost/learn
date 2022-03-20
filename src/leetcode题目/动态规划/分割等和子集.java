ac4d69a21115f9b58e1bd9dbf54769027dcdcf6c
hello <pzghost123@gmail.com>
2022-03-20 13:31:21 +0800

    leetcode

--- src/leetcode题目/动态规划/分割等和子集.java
+++ src/leetcode题目/动态规划/分割等和子集.java
@@ -19,7 +19,35 @@ public class 分割等和子集 {
     public static void main(String[] args) {
         int[] nums = {1, 5, 11, 5};
         //nums = new int[]{1, 2, 3, 5};
-        System.out.println(canPartition(nums));
+        System.out.println(canPartition02(nums));
+    }
+
+    public static boolean canPartition02(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            for (int j = target; j >= num; --j) {
+                dp[j] |= dp[j - num];
+            }
+        }
+        return dp[target];
     }
 
     public static boolean canPartition(int[] nums) {
