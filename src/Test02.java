ecce2d6210970a36ef505b00721c651ead9a2a5a
pengzhong <189968924@qq.com>
2024-12-16 18:25:38 +0800

    5

--- src/Test02.java
+++ src/Test02.java
@@ -1,44 +1,22 @@
+import java.util.Arrays;
 
 public class Test02 {
 
     public static void main(String[] args) {
-
-    }
-
-    /**
-     * https://leetcode-cn.com/problems/exchange-lcci/
-     * 配对交换
-     * 配对交换。编写程序，交换某个整数的奇数位和偶数位，尽量使用较少的指令（也就是说，
-     * 位0与位1交换，位2与位3交换，以此类推）。
-     * <p>
-     * 示例1:
-     * 输入：num = 2（或者0b10）
-     * 输出 1 (或者 0b01)
-     * <p>
-     * 示例2:
-     * 输入：num = 3
-     * 输出：3
-     */
-    public static int exchangeBits(int num) {
-        for (int i = 0; i < 30; i += 2) {
-            int a = num >> i & 1;
-            int b = num >> (i + 1) & 1;
-            if (a != b) {
-                num ^= 1 << i;
-                num ^= 1 << (i + 1);
+        int[] stock = {4, 6, 8, 2, 3, 4};
+        int low = 0;
+        int high = stock.length - 1;
+        while (low < high) {
+            int pivot = (low + high) / 2;
+            if (stock[pivot] > stock[high]) {
+                low = pivot + 1;
+            } else if (stock[pivot] < stock[high]) {
+                high = pivot;
+            } else {
+                high--;
             }
         }
-        return num;
-    }
-
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
-
+        System.out.println(low);
     }
 
 //    static class Node {
